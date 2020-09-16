f6734759262df9bd302b596a73cafc724dd63773
Yeung <yuyangli@andrew.cmu.edu>
2020-09-15 23:04:49 -0700

    temporary disabled login_required

--- app/routes.py
+++ app/routes.py
@@ -9,6 +9,8 @@ from werkzeug.urls import url_parse
 import json, requests
 import numpy as np
 import pickle
+from datetime import datetime
+from pytz import timezone
 
 from data import Match
 
@@ -39,7 +41,7 @@ def login():
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
-        if next_page == None or url_parse(next_page).netlog != '':
+        if next_page == None or url_parse(next_page).netloc != '':
             next_page = url_for('home')
         return redirect(next_page)
     return render_template('forms/login.html', form=form)
@@ -53,7 +55,7 @@ def logout():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        redirect(url_for('home'))
+        return redirect(url_for('home'))
     form = RegisterForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
@@ -71,27 +73,38 @@ def forgot():
     form = ForgotForm()
     return render_template('forms/forgot.html', form=form)
 
+@app.route('/abort')
+def abort():
+    return render_template('errors/abort.html')
+
 
 @app.route('/test')
-@login_required
+#@login_required
 def test():
+    today = datetime.now(timezone('Europe/London')).date()
     #with open('data/fixtures.json', 'r') as f:
     #    fixtures = json.load(f)['api']['fixtures']
     
+    # league_id for premier league 2020 ~ 2021: 2790
     url = "https://api-football-v1.p.rapidapi.com/v2/fixtures/league/2790/next/8"
 
     querystring = {"timezone":"Europe/London"}
 
     headers = {
         'x-rapidapi-host': "api-football-v1.p.rapidapi.com",
+        # provide your api key to api-football
         'x-rapidapi-key': "9a4cc675c7mshdaa84f5b192909ap1f9979jsn18ef7493b7db"
     }
 
     response = requests.request("GET", url, headers=headers, params=querystring)
+    
+    response_json = response.json()
+    if not response_json['api']['results']:
+        return redirect(url_for('abort'))
+    fixtures = response_json['api']['fixtures']
 
-    fixtures = response.json()['api']['fixtures']
-
-    return render_template('pages/match.html', fixtures=fixtures, N=len(fixtures))
+    return render_template('pages/match.html', fixtures=fixtures, 
+                           N=len(fixtures), date=today)
 
 @app.route('/predict', methods=['GET' ,'POST'])
 def predict():
